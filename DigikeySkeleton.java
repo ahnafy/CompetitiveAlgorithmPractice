b884d6ea5bfcc54ec998ff0f12027690443cf969
munnsmunns <mmunns16@gmail.com>
2018-10-17 19:42:56 -0500

    Update the skeleton with better IO handling
    Now it will work when we give it a file name, and will print the result to a file instead of standard output

--- DigikeySkeleton.java
+++ DigikeySkeleton.java
@@ -7,17 +7,24 @@ import java.lang.Math;
 
 public class Digikey {
 	public static void main(String args[]) {
+		Scanner input = null;
+        	PrintWriter output = null;
+        	try {
+	    		// IMPORTANT! change these for each problem!!
+            		input = new Scanner(new File("C:\DKC3\problemIn.txt"));
+// 			input = new Scanner(System.in); // for our own tests. make sure to comment out when done
+            		output = new PrintWriter("C:\DKC3\problemOut.txt");
+        	} catch (Exception e) { System.out.println("crap " + e); }
+		
 		String inputPath = ""; // change to actual path
 		String outputPath = "";
-		Scanner file = new Scanner(inputPath);
-		// Scanner file = new Scanner(System.in); // use standard input for testing ourselves
 
 		for (int testCase = 0; testCase < 10; testCase++) {
 
 			// CODE HERE
 
 
-			System.out.println("THE RESULT");
+			output.println("THE RESULT");
 		}
 	}
 
