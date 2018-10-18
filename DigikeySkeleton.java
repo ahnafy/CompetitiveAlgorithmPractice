94a9daaeb40c08fd4eaeda9e6153059ca768235f
munnsmunns <mmunns16@gmail.com>
2018-10-17 21:12:31 -0500

    Add changes suggested by nic and delete seconds class

--- DigikeySkeleton.java
+++ DigikeySkeleton.java
@@ -1,8 +1,7 @@
 package digikey;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.io*;
 import java.lang.Math;
 
 public class Digikey {
@@ -14,51 +13,21 @@ public class Digikey {
             		input = new Scanner(new File("C:\DKC3\problemIn.txt"));
 // 			input = new Scanner(System.in); // for our own tests. make sure to comment out when done
             		output = new PrintWriter("C:\DKC3\problemOut.txt");
-        	} catch (Exception e) { System.out.println("crap " + e); }
+        	} catch (Exception e) { System.out.println(e); }
 		
 		String inputPath = ""; // change to actual path
 		String outputPath = "";
 
 		for (int testCase = 0; testCase < 10; testCase++) {
-
-			// CODE HERE
-
-
-			output.println("THE RESULT");
+			try {
+				// CODE HERE
+				output.println("THE RESULT");
+				
+			} catch(Exception e) { output.println("Bogus output"); } // change this to reflect the question (if there is time)
 		}
 		output.close();
 		input.close();
 	}
 
-
-}
-
-class Seconds {
-	public static void main(String args[]) {
-		String inputPath = ""; // change to actual path
-		String outputPath = "";
-		//		Scanner file = new Scanner(inputPath);
-		Scanner file = new Scanner(System.in); // use standard input for testing ourselves
-
-		for (int testCase = 0; testCase < 10; testCase++) {
-			try {
-				int hours = file.nextInt();
-				int minutes = file.nextInt();
-				int seconds = file.nextInt();
-
-				if(hours >= 12) {
-					hours %= 12;
-				}
-
-				int sum = (hours * 60 * 60) + (minutes * 60) + seconds;
-
-
-				System.out.println(sum);
-			}
-			catch(Exception e) {
-				System.out.println(e);
-			}
-		}
-	}
 
 }
